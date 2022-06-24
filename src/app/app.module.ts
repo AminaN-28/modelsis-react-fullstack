cc07923112084ced5820632910fd1e7f02b3090e
Amina <macbook@10.0.210.129>
2022-06-24 12:07:13 +0000

    Mon projet Best Effort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AddProductTypeComponent } from './add-product-type/add-product-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateProductComponent,
+    AddProductComponent,
+    AddProductTypeComponent
   ],
   imports: [
     BrowserModule,
